9effc2dd3325e9f96bb4794033a3ff88bff110c5
Keith Moore <keith.m4@gmail.com>
2016-07-09 09:07:28 -0400

    added more projects from old portfolio; changed back to thumbnail-grid layout

--- index.php
+++ index.php
@@ -14,10 +14,9 @@
 	<link rel="stylesheet" href="css/normalize.css">
 	<link rel="stylesheet" href="css/skeleton.css">
 	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="css/portfolio.css">
 	<link rel="stylesheet" href="css/cd-top.css">
 	<link rel="stylesheet" href="css/font-awesome.min.css">
-
-	<link rel="icon" href="images/favicon.ico">
 
 </head>
 <body>
@@ -28,7 +27,7 @@
 				<div class="six columns splash">
 					<h2>Keith Moore</h2>
 					<p>I'm a full stack web developer with a focus on JavaScript, the MEAN stack, and the LAMP stack.</p>
-					<a class="button button-primary spaced-button-left" href="#portfolio">Browse Portfolio</a><a class="button button-primary spaced-button-right" href="/kmoore_resume.pdf">Download Resume</a>
+					<a class="button button-primary spaced-button-left" href="#portfolio">Browse Portfolio</a><a class="button button-primary spaced-button-right" href="kmoore_resume.pdf">Download Resume</a>
 				</div>
 			</div>
 		</div>
@@ -63,33 +62,100 @@
 			<div class="header-wrapper">
 				<h2>Projects</h2>
 			</div>
-			<div class="row">
-				<div class="four columns">
-					<h4>Ag Labs</h4>
-					<div class="u-max-full-width">	
-						<img class="u-full-width" src="images/ag.png">
+			<div class="row portfolio-wrapper">
+				<div class="thumb-wrapper">
+					<div class="outer-thumb">
+						<img id="img-1" class="portfolio-thumb" src="images/coffee.png" alt="Coffee Site">
 					</div>
-					<p>eCommerce site built using AngularUI Router, Express/Node.js, and MySQL.</p>
-					<a class="button" href="https://github.com/kdavidmoore/final-project">GitHub</a>
-					<a class="button" href="http://ag.kdavidmoore.com/">Demo</a>
+					<div class="hover-thumb">
+						<h4 class="thumb-header">Coffee Site</h4>
+						<p class="portfolio-buttons"><a href="https://github.com/kdavidmoore/coffee2-angular" class="portfolio button">GitHub</a> <a href="http://kdavidmoore.com/coffee" class="portfolio button">Demo</a></p>
+						<p class="info-text">An eCommerce app built using AngularJS, Node.js/Express, and MongoDB.</p>
+					</div>
 				</div>
-				<div class="four columns">
-					<h4>Interactive Map</h4>
-					<div class="u-max-full-width">
-						<img class="u-full-width" src="images/langs.png">
+				<div class="thumb-wrapper">
+					<div class="outer-thumb">
+						<img id="img-2" class="portfolio-thumb" src="images/ag.png" alt="Ag Labs">
 					</div>
-					<p>Interactive map built using DataMaps.js, AngularJS, and ngRouter.</p>
-					<a class="button" href="https://github.com/kdavidmoore/backTableBankers">GitHub</a>
-					<a class="button" href="http://kdavidmoore.com/languages">Demo</a>
+					<div class="hover-thumb">
+						<h4 class="thumb-header">Ag Labs</h4>
+						<p class="portfolio-buttons"><a href="https://github.com/kdavidmoore/final-project" class="portfolio button">GitHub</a> <a href="http://ag.kdavidmoore.com" class="portfolio button">Demo</a></p>
+						<p class="info-text">An eCommerce app built using AngularUI Router, Node.js/Express, and MySQL.</p>
+					</div>
 				</div>
-				<div class="four columns">
-					<h4>Rock Talk</h4>
-					<div class="u-max-full-width">
-						<img class="u-full-width" src="images/yikyak.png">
+				<div class="thumb-wrapper">
+					<div class="outer-thumb">
+						<img id="img-7" class="portfolio-thumb" src="images/yikyak.png" alt="Rock Talk">
 					</div>
-					<p>Social media app built using PHP, AngularJS, MySQL, and MeekroDB.</p>
-					<a class="button" href="https://github.com/kdavidmoore/yikyak-clone">GitHub</a>
-					<a class="button" href="http://rocks.kdavidmoore.com">Demo</a>
+					<div class="hover-thumb">
+						<h4 class="thumb-header">Rock Talk</h4>
+						<p class="portfolio-buttons"><a href="https://github.com/kdavidmoore/yikyak-clone" class="portfolio button">GitHub</a> <a href="http://rocks.kdavidmoore.com" class="portfolio button">Demo</a></p>
+						<p class="info-text">Social media app built using PHP, MySQL, and MeekroDB.</p>
+					</div>
+				</div>
+			</div>
+			<div class="row portfolio-wrapper">
+				<div class="thumb-wrapper">
+					<div class="outer-thumb">
+						<img id="img-4" class="portfolio-thumb" src="images/langs.png" alt="Interactive Map">
+					</div>
+					<div class="hover-thumb">
+						<h4 class="thumb-header">Interactive Map</h4>
+						<p class="portfolio-buttons"><a href="https://github.com/kdavidmoore/backTableBankers" class="portfolio button">GitHub</a> <a href="http://kdavidmoore.com/languages" class="portfolio button">Demo</a></p>
+						<p class="info-text">Uses AngularJS and DataMaps.js to apply and graphically display up to three data filters.</p>
+					</div>
+				</div>
+				<div class="thumb-wrapper">
+					<div class="outer-thumb">
+						<img id="img-5" class="portfolio-thumb" src="images/chat.png" alt="Node Chat">
+					</div>
+					<div class="hover-thumb">
+						<h4 class="thumb-header">Node Chat</h4>
+						<p class="portfolio-buttons"><a href="https://github.com/kdavidmoore/node-chat" class="portfolio button">GitHub</a> <a href="https://shielded-fortress-12118.herokuapp.com/" class="portfolio button">Demo</a></p>
+						<p class="info-text">Send and recieve messages from multiple chat clients using Node.js and socket.io.</p>
+					</div>
+				</div>
+				<div class="thumb-wrapper">
+					<div class="outer-thumb">
+						<img id="img-6" class="portfolio-thumb" src="images/tweets.png" alt="Panama Papers Tweets">
+					</div>
+					<div class="hover-thumb">
+						<h4 class="thumb-header">Panama Papers Tweets</h4>
+						<p class="portfolio-buttons"><a href="https://github.com/kdavidmoore/panama-papers-tweets" class="portfolio button">GitHub</a> <a href="http://kdavidmoore.com/panama-papers" class="portfolio button">Demo</a></p>
+						<p class="info-text">Pulls data from Twitter API and filters results using AngularJS.</p>
+					</div>
+				</div>
+			</div>
+			<div class="row portfolio-wrapper">
+				<div class="thumb-wrapper">
+					<div class="outer-thumb">
+						<img id="img-8" class="portfolio-thumb" src="images/calc.png" alt="JS Calculator">
+					</div>
+					<div class="hover-thumb">
+						<h4 class="thumb-header">JS Calculator</h4>
+						<p class="portfolio-buttons"><a href="https://github.com/kdavidmoore/js-calculator" class="portfolio button">GitHub</a> <a href="http://kdavidmoore.com/calculator" class="portfolio button">Demo</a></p>
+						<p class="info-text">Performs and displays simple calculations using HTML, CSS, jQuery, and JavaScript</p>
+					</div>
+				</div>
+				<div class="thumb-wrapper">
+					<div class="outer-thumb">
+						<img id="img-8" class="portfolio-thumb" src="images/places.png" alt="Search Places">
+					</div>
+					<div class="hover-thumb">
+						<h4 class="thumb-header">Search Places</h4>
+						<p class="portfolio-buttons"><a href="https://github.com/kdavidmoore/us-cities-angular" class="portfolio button">GitHub</a> <a href="http://kdavidmoore.com/search-places" class="portfolio button">Demo</a></p>
+						<p class="info-text">Search for places and display directions using AngularJS and Google Maps JavaScript API.</p>
+					</div>
+				</div>
+				<div class="thumb-wrapper">
+					<div class="outer-thumb">
+						<img id="img-3" class="portfolio-thumb" src="images/movies.png" alt="Movie Crawler">
+					</div>
+					<div class="hover-thumb">
+						<h4 class="thumb-header">Movie Crawler</h4>
+						<p class="portfolio-buttons"><a href="https://github.com/kdavidmoore/movie-app" class="portfolio button">GitHub</a> <a href="http://kdavidmoore.com/movie-crawler" class="portfolio button">Demo</a></p>
+						<p class="info-text">Pulls JSON data from themoviedb.org API; filters results using typeahead.js and Isotope.</p>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -154,12 +220,7 @@
 <script src="http://cdn.jsdelivr.net/jquery.scrollto/2.1.2/jquery.scrollTo.min.js"></script>
 <script src="http://cdn.jsdelivr.net/jquery.localscroll/1.4.0/jquery.localScroll.min.js"></script>
 <script src="js/cd-top.js"></script>
-<script type="text/javascript">
-	$(document).ready(function() {
-		// binds localSroll to all local links
-		$.localScroll();
-	});
-</script>
-
+<script src="js/jquery.matchHeight.js"></script>
+<script src="js/initPage.js"></script>
 </body>
 </html>
---
+++ js/initPage.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+	// binds localSroll to all local links
+	$.localScroll();
+});
+
+$(window).load(function(){
+	// make all the thumbnail images the same height
+	$('.portfolio-thumb').matchHeight({
+		byRow: true,
+		property: 'height',
+		target: null,
+		remove: false
+	});
+	// show more info on mouseover
+	$(".thumb-wrapper").mouseenter(function(){
+		$(this).find('.hover-thumb').fadeToggle("fast");
+	});
+	// hide info on mouseleave
+	$(".thumb-wrapper").mouseleave(function(){
+		$(this).find('.hover-thumb').fadeToggle("fast");
+	});
+	// set the thumb-wrapper width/height equal to the width/height of one of the images
+	var setHeight = $("#img-1").height();
+	$('.thumb-wrapper').height(setHeight);
+	// when the window is resized, tell matchHeight to update
+	$(window).resize(function() {
+		$.fn.matchHeight._update();
+	});
+});
